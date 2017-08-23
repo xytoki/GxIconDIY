7032ce8148ce0161d0fe87da7a9d7ad6849b379e
Weng <570359547@qq.com>
2017-08-23 19:30:11 +0800

    Add AutoInjector Tool

---
+++ autoInjectionTool/autoInjectionTool.js
@@ -0,0 +1,39 @@
+var exporter="http://gxicon.e123.pw/api.php?icon/export";
+var folder="app";
+
+var fs = require('fs');
+var path=require("path");
+var wget = require('node-wget');
+var request = require('request');
+var drawable_folder=path.normalize(__dirname+"/../"+folder+"/src/main/res/drawable-nodpi");
+var drawable_xml=path.normalize(__dirname+"/../"+folder+"/src/main/res/xml/drawable.xml");
+var iconpack_xml=path.normalize(__dirname+"/../"+folder+"/src/main/res/values/icon_pack.xml");
+console.log(drawable_folder);
+console.log(drawable_xml);
+console.log(iconpack_xml);
+request(exporter, function (error, response, body) {
+	if(error){
+		throw error;
+		process.exit(1);
+	}
+	var j=JSON.parse(body).icons;
+	console.log("Ready to download...");
+	var next=function(i,cb){
+		if(typeof(j[i])=="undefined")return cb();
+		var basefn=j[i][0]+"_"+j[i][1];
+		var fn=drawable_folder+"/"+basefn+path.extname("http:"+j[i][2]);
+		wget({url: "http:"+j[i][2], dest: fn}, function(){
+			//Write drawable.xml
+			var dx=fs.readFileSync(drawable_xml).toString().split("<!--AutoInjector End-->");
+			fs.writeFileSync(drawable_xml,dx[0]+'	<item drawable="'+basefn+'" />\r\n	<!--AutoInjector End-->'+dx[1]);
+			//Write icon_pack.xml
+			var dx=fs.readFileSync(iconpack_xml).toString().split("<!--AutoInjector End-->");
+			fs.writeFileSync(iconpack_xml,dx[0]+'	<item>'+basefn+'</item>\r\n	<!--AutoInjector End-->'+dx[1]);
+			//Done
+			console.log(fn);
+			next(i+1,cb)
+		});
+	}
+	next(0,function(){
+	})
+});
