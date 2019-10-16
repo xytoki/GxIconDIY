7bfcdc67af0383fc1a72c98731b3a0d95c837d56
xyToki <7547189+xytoki@users.noreply.github.com>
2019-10-16 11:54:56 +0000

    add html and socket.io

--- buildServer.js
+++ buildServer.js
@@ -15,8 +15,8 @@ const http=require('http');
 const log=require('npmlog');
 const Git = require('simple-git/promise')();
 const uuid=require("uuid/v4");
-const exec = require('child-process-promise').exec;
 const pty = require('node-pty');
+const static = require('koa-static');
 
 var q = queue();
 q.autostart=true;
@@ -34,11 +34,13 @@ q.on("error",function(err){
     q.current=false;
 });
 var finishedJobs=[];
+var socketPool={};
 
 const app = new Koa();
 app.use(cors());
 app.use(kjson());
 app.use(bodyParser());
+app.use(static(__dirname+'./buildServerAssets'));
 const server = http.Server(app.callback());
 const io = IO(server);
 var port=process.env.PORT || 5656;
@@ -83,7 +85,26 @@ router.get('/api/queue', async (ctx, next) => {
 router.get('/', async (ctx, next) => {
     ctx.response.body = 'GxIcon BuildServer [beta]<br>Current pending jobs:<br>';
 });
+router.get('/terminal', async (ctx, next) => {
+    ctx.response.body = 'GxIcon BuildServer [beta]<br>Current pending jobs:<br>';
+});
 app.use(router.routes());
+io.on('connection', function(socket){
+    socket.on("register",function(id){
+        socket._id=id;
+        socket._uniq=uuid();
+        socketPool[id]=socketPool[id]||[];
+        socketPool[id].push(socket);
+        socket.emit("status","connected");
+    })
+    socket.on('disconnect', function(){
+        for(var i in socketPool[socket._id]){
+            if(socketPool[socket._id]._uniq==socket._uniq){
+                socketPool[socket._id].splice(i,1);
+            }
+        }
+    });
+});
 function addJob(data){
     return (function(){
         data.localId=uuid();
@@ -91,12 +112,22 @@ function addJob(data){
         var f=async function () {
             var data=arguments.callee._gx_info;
             data.execTime=new Date().getTime();
-            await resetEnv(process.stdout.write);
+            ioSend(data.localId,"status","prepare");
+            await resetEnv(function(){
+                ioSend(data.localId,"message",data);
+            });
+            ioSend(data.localId,"status","config");
             await fsex.writeFile("_autoMake.json",JSON.stringify(data.config, null, 4));
-            await execMake(process.stdout.write);
+            ioSend(data.localId,"status","build");
+            await execMake(function(){
+                ioSend(data.localId,"message",data);
+            });
+            ioSend(data.localId,"status","sign");
+            ioSend(data.localId,"status","upload");
             data.endTime=new Date().getTime();
             finishedJobs.push(data);
             log.info("JOB","END",{localId:data.localId,jobId:data.jobId});
+            ioSend(data.localId,"status","done");
         };
         f._gx_info=data;
         log.info("JOB","ADD",{localId:data.localId,jobId:data.jobId});
@@ -118,16 +149,25 @@ async function execMake(write){
     await ptySpawn('node',['autoMake'],write);
     await ptySpawn('./gradlew',['assembleRelease'],write);
 }
-async function ptySpawn(cmd,arg,data){
+async function ptySpawn(cmd,arg,datawrite){
     return new Promise(function(resolve,reject){
+        log.info("CMD",cmd,arg);
         var ptyProcess = pty.spawn(cmd,arg, {
             name: 'xterm-color',
             cols: 80,
             rows: 30,
             cwd: __dirname,
             env: process.env
-          });
-          ptyProcess.on('data',data);
-          ptyProcess.on('exit',resolve);
+        });
+        ptyProcess.on('data',function(data){
+            datawrite(data);
+        });
+        ptyProcess.on('exit',resolve);
     });
+}
+function ioSend(id,ev,content){
+    if(typeof(socketPool[id])=="undefined")return;
+    for(var i in socketPool[id]){
+        socketPool[id].emit(ev,content);
+    }
 }
